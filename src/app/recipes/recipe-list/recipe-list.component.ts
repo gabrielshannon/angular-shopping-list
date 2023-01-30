97dbb2e84e723dad464c376eeeae708118ceedaa
gabrielshannonsocius <113363825+gabrielshannonsocius@users.noreply.github.com>
2023-01-30 20:45:01 +0000

    recipe service complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,17 +1,21 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css'],
+  styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-  recipes: Recipe[];
-  constructor(private recepieService: RecipeService) {}
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] 
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    this.recipes = this.recepieService.getRecipes();
+    this.recipes = this.recipeService.getRecipes();
   }
+
+
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,21 @@
-import { EventEmitter } from "@angular/core";
-import { Recipe } from "./recipe.model";
-
+import { EventEmitter } from '@angular/core';
+import { Recipe } from './recipe.model';
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
-    
-    private recipes: Recipe[] = [
-        new Recipe(
-          'Test Recipe',
-          'Simply a Test!',
-          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
-        ),
-        new Recipe(
-          'Test Recipe 2',
-          'Simply a Test!',
-          'https://www.stylist.co.uk/images/app/uploads/2021/06/08183259/quick-lasagne-recipe-crop-1623173675-1910x1910.png?w=256&h=256&fit=max&auto=format%2Ccompress'
-        ),
-      ];
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+  ];
 
-      getRecipes() {
-        return this.recipes.slice();
-      }
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
