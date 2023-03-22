b3da0269d3cdb4e16142dda7d1434b1defa7d0fd
Gabriel Shannon <gabrielshannon96@gmail.com>
2023-03-22 20:43:41 +0000

    not working lol

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -14,6 +14,6 @@ export class HeaderComponent {
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipies();
+    this.dataStorageService.fetchRecipies().subscribe();
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { map, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -20,10 +21,23 @@ export class DataStorageService {
   }
 
   fetchRecipies() {
-    this.http.get<Recipe[]>('https://course-project-6db03-default-rtdb.firebaseio.com/recipes.json',
+   return this.http
+      .get<Recipe[]>(
+        'https://course-project-6db03-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
 
-    ).subscribe(recipes => {
-      this.recipeService.setRecipes(recipes)
-    })
+        })
+      )
+  
   }
 }
